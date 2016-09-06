1fce92d5187c9407521bb96a6f329a8c7bd31f78
Jon Gretarsson <jgretarsson@salesforce.com>
2016-09-06 14:56:18 -0700

    minor(Comments) Remove dead comments

--- src/main/java/com/relateiq/annotations/EncryptionScope.java
+++ src/main/java/com/relateiq/annotations/EncryptionScope.java
@@ -8,9 +8,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * See {@link com.relateiq.mongo.EncryptAtRestInterceptor}.
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
 public @interface EncryptionScope {
